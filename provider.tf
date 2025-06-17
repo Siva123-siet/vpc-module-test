4e8f21f82de38af6b28a64332551b73f393016ad
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-18 00:32:22 +0530

    terraform

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+  backend "s3" {
+    bucket = "84sremotestate"
+    key    = "vpc-module-test"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+    #dynamodb_table = "84sremotestate"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
